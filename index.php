a14a54f6adc0b2a4ad07c366b154b272307686fe
dahalprastut <dahalprastut@gmail.com>
2019-02-12 00:04:37 +0545

    second commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>  
+
+    <!-- home page start -->
+    <div class="home-page">
+        <nav>
+            <div class="center clearfix">
+                <a href="#" class="left">
+                    <img src="images/FinalLogoHigh.png" alt="logo">
+                </a>
+                <ul class="clearfix">
+                    <li><a class="active" href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Our Services</a></li>
+                    <li><a href="#">Our Client</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+                <div class="hidden-menu">
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+        </nav>
+
+        <header>
+            <h2>EXPERIENCE THE <br>FUTURE</h2>
+            <a href="#" class="btn">Read More</a>
+        </header>
+
+    </div>
+
+    <!-- home page ends -->
+
+    <main>
+        <div class="center">
+            sdafasdf
+            
+        </div>
+    </main>
+
+
+    <script src='js/main.js'>
+    
+    </script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// for menu
+var selectMenu = document.querySelector('.hidden-menu');
+selectMenu.onclick=function(){
+    var selectUl = document.querySelector('nav ul');
+    selectMenu.classList.toggle('hidden-color-changed');
+    selectUl.classList.toggle('shown-ul');
+
+}
+
+// menu ends here
+
